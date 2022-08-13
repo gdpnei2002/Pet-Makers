d7a6509d7b6fe34783598da0409bca14cff11095
neilson <joseneilson25@gmail.com>
2022-08-12 23:48:24 -0300

    inputs + css

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,54 @@
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [values, setValues] = useState();
+
+  const handleChangeValues = (value) =>{
+    setValues((prevValue)=>({
+      ...prevValue,
+      [value.target.name]: value.target.value,
+    }));
+  }
+
+  const handleClickButton = () =>{
+    console.log(values);
+  }
+
   return (
-    <div className="App">
-      <p>Hello world</p>
+    <div className="app-container">
+      <h1 className="register-title">Pet-Makers</h1>
+          <input 
+            type="text" 
+            name="name" 
+            placeholder="Nome" 
+            className="register-input"
+            onChange={handleChangeValues}
+            />
+          <input 
+            type="text" 
+            name="age" 
+            placeholder="Idade" 
+            className="register-input"
+            onChange={handleChangeValues}
+            /> <br />
+
+          <input 
+            type="select" 
+            name="breed" 
+            placeholder="Raça" 
+            className="register-input"
+            onChange={handleChangeValues}
+            />
+            <select
+              className="register-input"
+              name="tipo" >
+              <option value="cachorro">Cachorro</option>
+              <option value="gato">Gato</option>
+            </select>
+        
+          <button className="register-button" 
+            onClick={()=>handleClickButton()}>Cadastrar</button>
     </div>
   );
 }
