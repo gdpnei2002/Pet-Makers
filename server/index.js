12aac86618dcea67e1c735f59f230c8b36d998cf
neilson <joseneilson25@gmail.com>
2022-08-13 00:03:49 -0300

    teste de mysql

--- server/index.js
+++ server/index.js
@@ -1,5 +1,22 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql")
+
+const db = mysql.createPool({
+    host:"localhost",
+    user: "root",
+    password:"1010",
+    database: "pets"
+});
+
+app.get("/", (req, res) =>{
+    let SQL = 
+    "INSERT INTO pet (name, age, breed) VALUES ('bob', '4', 'labrador')";
+
+    db.query(SQL),(err, result) =>{
+        console.log(err)
+    }
+});
 
 app.listen(3001, ()=>{
     console.log("rodando servidor");
